289653c1746459e1d675ec4de7e7d9bf8742518c
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2024-06-25 21:42:43 +0300

    basic syntax

--- 01.JS_Syntax_Fundamentals_Lab/b_basic_syntax.js
+++ 01.JS_Syntax_Fundamentals_Lab/b_basic_syntax.js
@@ -1,11 +1,11 @@
 // variable declaration
-let age = 20
-let firstName = 'Gosho'
-let isMale = true
+let age = 20;
+let firstName = 'Gosho';
+let isMale = true;
 
-console.log(age)
-console.log(firstName)
-console.log(isMale)
+console.log(age);
+console.log(firstName);
+console.log(isMale);
 
 
 // basic solve function
@@ -13,6 +13,7 @@ function solve (num1, num2) {
     console.log(num1 + num2);
 
 }
+// execute function
 solve(1,2);
 
 // print on the console
@@ -21,5 +22,34 @@ console.log(`My name is, ${firstName}!`);
 
 // fix numbers
 let num1 = 0.1;
-let num2 = 0.2
-console.log(num1 + num2)
+let num2 = 0.2;
+console.log(num1 + num2);
+
+let num3 = 3;
+let num4 = 3.14;
+console.log(num3.toFixed(2));
+console.log(num4.toFixed(2));
+
+// using const
+const lastName = 'Rachev';
+
+// Block scope
+if (true) {
+    let schoolName = 'SoftUni'; // variable has block scope
+    console.log(schoolName);
+}
+// console.log(schoolName); // throws an error
+
+// Outer block scope
+if (true) {
+    let outerVariable = 100; // variable has block scope
+    if (true) {
+        let innerVariable = 200; // variable has block scope
+        console.log(outerVariable)
+    }
+    console.log(outerVariable);
+
+    // console.log(innerVariable); // throws an error
+
+}
+
---
+++ 01.JS_Syntax_Fundamentals_Lab/c-data-types.js
@@ -0,0 +1,27 @@
+//Number
+
+let integerNumber = 10;
+let decimalNumber = 3.14;
+let negativeIntegerNumber = -10;
+let maxPreciseNumber = Number.MAX_SAFE_INTEGER;
+let maxDoublePrecisionNumber = Number.MAX_VALUE;
+let notANumber = NaN;
+let infinity = Infinity;
+let negativeInfinity = -Infinity;
+
+//String
+let singleQuoteLiteral = 'single quote';
+let doubleQuoteLiteral = "double quote";
+let backTickedString = `back tick`;
+
+// Undefined
+let undf = undefined;
+
+// Null
+let empty = null;
+
+// BigInt
+let bigInt = BigInt('1234567890123456789012345678901234567890');
+console.log(bigInt)
+let bigInt2 = 1234567890123456789012345678901234567890n;
+console.log(bigInt2)
